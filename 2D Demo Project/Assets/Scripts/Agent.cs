b46b8383168ea939ea898c48ebf30ed0f314b55c
erincascioli <edcigm@rit.edu>
2025-04-15 12:07:47 -0400

    Implemented Flee method in Agent class

--- 2D Demo Project/Assets/Scripts/Agent.cs
+++ 2D Demo Project/Assets/Scripts/Agent.cs
@@ -13,8 +13,9 @@ public abstract class Agent : MonoBehaviour
     [SerializeField]
     protected Vector3 acceleration;
 
-    // Speed for movement
+    // Maximums for movement
     public float maxSpeed;
+    public float maxForce;
 
     void Start()
     {
@@ -32,19 +33,27 @@ public abstract class Agent : MonoBehaviour
         // Calculate a steering force
         Vector3 steeringForce = CalcSteeringForce();
 
+        // Limited the steering force to a max
+        steeringForce = Vector3.ClampMagnitude(steeringForce, maxForce);
+
         // Basic "movement" for agents
         acceleration += steeringForce;
         velocity += acceleration * Time.deltaTime;
         transform.position += velocity * Time.deltaTime;
     }
 
-    // ADD ALL STEERING METHODS HERE!
+    /// <summary>
+    /// Calculate a steering force to move an Agent toward a desired position
+    /// in the world.
+    /// </summary>
+    /// <param name="targetPosition">Specified position to seek</param>
+    /// <returns>Steering force for seeking</returns>
     public Vector3 Seek(Vector3 targetPosition)
     {
         Vector3 seekingForce = Vector3.zero;
 
         // Calculate a desired velocity (point toward the target)
-        Vector3 desiredVelocity = targetPosition - velocity;
+        Vector3 desiredVelocity = targetPosition - transform.position;
 
         // Scale to maximum speed
         desiredVelocity = desiredVelocity.normalized * maxSpeed;
@@ -55,11 +64,37 @@ public abstract class Agent : MonoBehaviour
         return seekingForce;
     }
 
-    // Overloaded Seek
+    /// <summary>
+    /// Calculate a steering force to move an Agent toward a desired GameObject
+    /// in the world.
+    /// </summary>
+    /// <param name="target">GameObject to seek</param>
+    /// <returns>Steering force for seeking</returns>
     public Vector3 Seek(GameObject target)
     {
         return Seek(target.transform.position);
     }
 
 
+    /// <summary>
+    /// Calculate a steering force to move an Agent away from a desired position
+    /// in the world.
+    /// </summary>
+    /// <param name="targetPosition">Specified position to flee</param>
+    /// <returns>Steering force for fleeing</returns>
+    public Vector3 Flee(Vector3 targetPosition)
+    {
+        Vector3 fleeingForce = Vector3.zero;
+
+        // Calculate a desired velocity (point toward the target)
+        Vector3 desiredVelocity = transform.position - targetPosition;
+
+        // Scale to maximum speed
+        desiredVelocity = desiredVelocity.normalized * maxSpeed;
+
+        // Calculate the seeking force needed
+        fleeingForce = desiredVelocity - velocity;
+
+        return fleeingForce;
+    }
 }
